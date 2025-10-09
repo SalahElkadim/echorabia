50a2afd05118742283104fdff13c27e3ef967b54
unknown <khaledgaber494@gmail.com>
2025-10-09 09:54:31 +0300

    payment

--- payment/urls.py
+++ payment/urls.py
@@ -3,7 +3,7 @@ from .views import CreatePaymentView,payment_callback_view,moyasar_webhook, paym
 
 urlpatterns = [
     path("create/", CreatePaymentView.as_view(), name="create-payment"),
-    path('pay/', payment_page, name='payment_page'),
+    path("pay/<int:booking_id>/", payment_page, name="payment_page"),
     path('callback/', payment_callback_view, name='payment-callback'),
     path('webhook/', moyasar_webhook, name='moyasar-webhook'),
 
