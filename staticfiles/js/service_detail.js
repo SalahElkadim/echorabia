d527c538734af78f341f22d89ca9192eaa6e8d8f
unknown <khaledgaber494@gmail.com>
2025-11-07 08:33:57 +0200

    sss

--- staticfiles/js/service_detail.js
+++ staticfiles/js/service_detail.js
@@ -32,18 +32,14 @@
 
 
   const adultsInput = document.querySelector('input[name="adults"]');
-const totalPriceEl = document.getElementById("total-price");
-if (totalPriceEl) {
-  const basePrice = parseFloat(totalPriceEl.dataset.basePrice);
-  console.log("Base price:", basePrice);
-} else {
-  console.warn("⚠️ Element with id='total-price' not found!");
-}
-  const basePrice = parseFloat(totalPriceEl.dataset.basePrice);
+//const totalPriceEl = document.getElementById("total-price");
+//if (totalPriceEl) {
+// const basePrice = parseFloat(totalPriceEl.dataset.basePrice);
+// console.log("Base price:", basePrice);} else {console.warn("⚠️ Element with id='total-price' not found!");}
+//const basePrice = parseFloat(totalPriceEl.dataset.basePrice);
 
-  adultsInput.addEventListener("input", () => {
-    const numAdults = parseInt(adultsInput.value) || 0;
-    const total = basePrice * numAdults;
-    totalPriceEl.textContent = total.toLocaleString(); // لإظهارها بصيغة مفهومة
-  });
+  //adultsInput.addEventListener("input", () => {
+   // const numAdults = parseInt(adultsInput.value) || 0;
+   // const total = basePrice * numAdults;
+   // totalPriceEl.textContent = total.toLocaleString(); // لإظهارها بصيغة مفهومة});
 
